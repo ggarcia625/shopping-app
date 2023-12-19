31f121da9a1684eaee16b5bc22186168238a1cc0
Greg Garcia <greggarcia426@gmail.com>
2023-12-18 17:51:18 -0700

    fixing merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Store from './pages/Store';
 import Navbar from './components/Navbar'
-import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import { ShoppingCartProvider } from "./context/shoppingCartContext";
 
 
 function App() {
